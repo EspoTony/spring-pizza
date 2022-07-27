5edf11cf1b3fd4e0d87c3b95a4046cc2decaa57b
EspoTony <espotony98@gmail.com>
2022-07-27 15:53:36 +0200

    esercitazione di giovedi

--- src/main/java/jana60/repository/PizzaRepository.java
+++ src/main/java/jana60/repository/PizzaRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import jana60.model.Pizza;
 
 public interface PizzaRepository extends CrudRepository<Pizza , Integer> {
+    public Integer countByNameAllIgnoreCase(String name);
 }
